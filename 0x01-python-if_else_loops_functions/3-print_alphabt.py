f61269ac79a64e1048072104e52430cf18b3f742
Dayvid007 <sorunkedavid18@gmail.com>
2023-12-24 12:34:11 -0800

    Tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
+i = 96
+while i < 123:
+    i += 1
+    if i == 101 or i == 113:
         continue
-    print(chr(i).format(), end="")
+    if i == 123:
+        break
+    print(chr(i).format(), end = "")
